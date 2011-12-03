88e778466a0024a14d48ad281c78a74ce79ec220
Denis Gorbachev <Denis.Gorbachev@trueoffice.ru>
2011-12-03 23:36:11 +0400

    (Re)Implemented rsLoadWordsTask

---
+++ lib/task/rsLoadWordsTask.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class rsLoadWordsTask extends sfBaseTask {
+	protected function configure() {
+		parent::configure();
+		
+		$this->addOptions(array(
+			new sfCommandOption('file', 'f', sfCommandOption::PARAMETER_REQUIRED, 'File to load', sfConfig::get('sf_data_dir').'/words'),
+		));
+
+		$this->namespace = 'rs';
+		$this->name = 'load-words';
+		$this->briefDescription = '';
+		$this->detailedDescription = '';
+	}
+
+	protected function execute($arguments = array(), $options = array()) {
+        $this->databaseManager = new sfDatabaseManager($this->configuration);
+        $this->databaseManager->getDatabase('doctrine')->getConnection();
+        $connection = Doctrine_Manager::connection();
+        $table = WordTable::getInstance();
+
+        $connection->getDbh()->query('TRUNCATE TABLE '.$table->getTableName());
+        $connection->beginTransaction();
+        $i = 0;
+        $fp = fopen($options['file'], 'r');
+		while ($name = mb_strtoupper(str_replace("\n", '', fgets($fp)))) {
+            $word = new Word();
+            $word->setName($name);
+            $word->save();
+            $word->free(true);
+            $i++;
+            if ($i % 100 == 0) {
+                $this->logSection($this->namespace, '['.$name.'] Loaded '.$i.' words');
+            }
+        }
+        $connection->commit();
+	}
+	
+}
