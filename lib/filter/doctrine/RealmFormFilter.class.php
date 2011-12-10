ee48f8f7376463d8c59bf1eaf8015972f35e0c9b
Denis Gorbachev <Denis.Gorbachev@trueoffice.ru>
2011-12-10 14:22:52 +0400

    Added realms to schema

---
+++ lib/filter/doctrine/RealmFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Realm filter form.
+ *
+ * @package    robotics
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class RealmFormFilter extends BaseRealmFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/RealmForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Realm form.
+ *
+ * @package    robotics
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class RealmForm extends BaseRealmForm
+{
+  public function configure()
+  {
+  }
+}
