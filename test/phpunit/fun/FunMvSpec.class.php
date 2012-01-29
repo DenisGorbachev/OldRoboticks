bfed47bea0c2920262a4448f2849b8c6e59b31c0
Denis Gorbachev <Denis.Gorbachev@trueoffice.ru>
2012-01-29 21:36:01 +0400

    Implemented fun:mv command

---
+++ test/phpunit/fun/FunMvSpec.class.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__.'/../../FunBaseSpec.class.php';
+
+class FunMvSpec extends FunBaseSpec {
+	public function testNormal() {
+		$this
+			->given('Genesis')
+				->and('User', 'Alice')
+				->and('Realm', 'Universe')
+                ->and('Robot', 'tea')
+			->when('Exec', 'fun:mv 0,0')
+			->then('Success')
+            ->then('Contains', '3,3')
+            ->then('Contains', '0,0')
+	;}
+
+    public function testStepping() {
+   		$this
+   			->given('Genesis')
+   				->and('User', 'Alice')
+   				->and('Realm', 'Universe')
+               ->and('Robot', 'tea')
+   			->when('Exec', 'fun:mv --steps 1 0,0')
+            ->then('Contains', '3,3')
+            ->then('NotContains', '0,0')
+            ->when('Exec', 'fun:mv --steps 1 0,0')
+            ->then('Contains', '0,0')
+            ->then('NotContains', '3,3')
+   	;}
+
+}
