4a1ef4fd6b4d143f05fbed02e45896d09ff4272d
Denis Gorbachev <Denis.Gorbachev@trueoffice.ru>
2012-03-27 00:07:24 +0400

    On my way to fun:wander command. Not as simple as I've thought.

--- client/lib/command/fun/FunWanderCommand.class.php
+++ client/lib/command/fun/FunWanderCommand.class.php
@@ -2,6 +2,7 @@
 
 require_once dirname(__FILE__) . '/../base/FunCommand.class.php';
 require_once dirname(__FILE__) . '/../MvCommand.class.php';
+require_once dirname(__FILE__) . '/../realm/RealmShowCommand.class.php';
 
 class FunWanderCommand extends FunCommand {
 	public function getParserConfig() {
@@ -34,55 +35,92 @@ class FunWanderCommand extends FunCommand {
 	}
 
     public function step($options, $arguments) {
+
+        // Accurate stepping needed. Tests would assure this.
+
         if (empty($this->cache['base']) || $this->cache['base'] != $this->getArgument('base')) {
+            $command = new RealmShowCommand($this->getConfig());
+            $result = $command->run();
             $localTarget = coords_string_to_array($this->getArgument('base'));
             $this->cache = array(
+                'realm' => $result['message']['arguments'],
                 'base' => $this->getArgument('base'),
                 'target' => $localTarget,
                 'local_target' => $localTarget,
-                'failed_targets' => array()
+                'local_target_reached' => false
             );
         }
-        if (empty($this->cache['target_reached'])) {
+        if (empty($this->cache['local_target_reached'])) {
             $command = new MvCommand($this->getConfig());
             $command->setArguments(array(
-                'sector' => $this->cache['target']
+                'sector' => $this->cache['local_target']
             ));
             $result = $command->run();
             if ($result['success']) {
-                if (isset($result['message']['arguments']['sector']) && coords_string_to_array($result['message']['arguments']['sector']) == $this->cache['target']) {
-                    $this->cache['target_reached'] = true;
+                if (isset($result['message']['arguments']['sector']) && coords_string_to_array($result['message']['arguments']['sector']) == $this->cache['local_target']) {
+                    $this->cache['local_target_reached'] = true;
                 }
-                $this->cache['failed_targets'] = array();
             } else {
-                $this->cache['failed_targets'][] = $this->cache['target'];
+                return false;
             }
         } else {
             $command = $this->getArgument('command');
             echo `$command`;
             $targetX = $this->cache['target']['x'];
             $targetY = $this->cache['target']['y'];
-            $diffX = $this->cache['target']['x'] - $this->cache['base']['x'];
-            $diffY = $this->cache['target']['y'] - $this->cache['base']['y'];
-            var_dump($diffX);
-            var_dump($diffY);
-            $increment = 2 * $this->getOption('range') + 1;
-            if (abs($diffX) > abs($diffY)) {
-                $targetY += ($diffX > 0? -1 : 1) * $increment;
-            } else if (abs($diffX) < abs($diffY)) {
-                $targetX += ($diffY > 0? 1 : -1) * $increment;
-            } else if ($diffX == $diffY) {
-                $targetY += ($diffX > 0? -1 : 1) * $increment;
-            } else if (abs($diffX) == $diffY) {
-                $targetY += $increment;
-            } else {
-                $targetX += ($diffY > 0? 1 : -1) * $increment;
-            }
-            $this->cache['target'] = array(
-                'x' => $targetX,
-                'y' => $targetY,
-            );
-            $this->cache['target_reached'] = false;
+            $baseX = $this->cache['base']['x'];
+            $baseY = $this->cache['base']['y'];
+            $iterations = 0;
+            do {
+                $halfTargetX = $targetX;
+                $halfTargetY = $targetX;
+                $diffX = $targetX - $baseX;
+                $diffY = $targetY - $baseY;
+                var_dump($diffX);
+                var_dump($diffY);
+                $increment = 2 * $this->getOption('range') + 1;
+                $halfIncrement = $this->getOption('range') + 1;
+                if (abs($diffX) > abs($diffY)) {
+                    $targetY += ($diffX > 0? -1 : 1) * $increment;
+                    $halfTargetX += ($diffX > 0? -1 : 1) * $halfIncrement;
+                    $halfTargetY = $targetY;
+                } else if (abs($diffX) < abs($diffY)) {
+                    $targetX += ($diffY > 0? 1 : -1) * $increment;
+                    $halfTargetX = $targetX;
+                    $halfTargetY += ($diffY > 0? -1 : 1) * $halfIncrement;
+                } else if ($diffX == $diffY) {
+                    $targetY += ($diffX > 0? -1 : 1) * $increment;
+                    $halfTargetX += ($diffX > 0? -1 : 1) * $halfIncrement;
+                    $halfTargetY += ($diffY > 0? -1 : 1) * $halfIncrement;
+                } else if ($diffX == abs($diffY)) {
+                    $targetX += ($diffY > 0? 1 : -1) * $increment;
+                    $halfTargetX += ($diffX > 0? -1 : 1) * $halfIncrement;
+                    $halfTargetY += ($diffY > 0? -1 : 1) * $halfIncrement;
+                } else {
+                    var_dump('ITERATION '.$iterations);
+                    $targetY += $increment;
+                    $halfTargetX += $targetX;
+                    $halfTargetY += $halfIncrement;
+                    $iterations++;
+                }
+                $target = array(
+                    'x' => $targetX,
+                    'y' => $targetY,
+                );
+                $halfTarget = array(
+                    'x' => $halfTargetX,
+                    'y' => $halfTargetY,
+                );
+                foreach (array($target, $halfTarget) as $localTarget) {
+                    if ($localTarget['x'] > 0 && $localTarget['x'] < $this->cache['realm']['width'] && $localTarget['y'] > 0 && $localTarget['y'] < $this->cache['realm']['height']) {
+                        $this->cache['target'] = $target;
+                        $this->cache['local_target'] = $localTarget;
+                        $this->cache['local_target_reached'] = false;
+                        return;
+                    }
+                }
+            } while ($iterations < 2);
+            return false;
         }
     }
 
