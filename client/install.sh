7cc79901baaa4003ff18df25566c240cd6228dce
Denis Gorbachev <Denis.Gorbachev@faster-than-wind.ru>
2012-04-21 18:39:03 +0400

    Added client installation script

---
+++ client/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $EUID -ne 0 ];
+then
+	echo "Failure: you should run this script as root."
+	exit 1
+fi
+
+CLIENT_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ ! -e /usr/bin/rk ];
+then
+	ln -s "$CLIENT_DIR/roboticks" /usr/bin/rk
+fi
+PRESENCE="$(grep -c robot /etc/bash.bashrc)"
+if [ $PRESENCE -eq "0" ];
+then
+	cat "$CLIENT_DIR/functions" >> /etc/bash.bashrc
+fi
+source "$CLIENT_DIR/functions"
+echo "Success: installed roboticks client."
